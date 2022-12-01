a4d2c4259ba48be462772cde69d8270a92d2a223
marvanG <marius.vangeli@hotmail.segit config --global user.name marvanG>
2022-12-01 10:51:25 +0100

    tutorial completed

---
+++ src/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+        <button @click="onClick()">
+            <img src="send_button_icon.png" style="width: 20px" >
+             {{text}}
+            
+        </button>
+        
+
+    
+
+   
+        
+
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+
+    },
+    methods: {
+        onClick() {
+            console.log('klickad')
+            this.$emit('place-order')
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+Button {
+    margin-left: 10px;
+    
+   
+
+}
+</style>
